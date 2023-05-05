e70c9b1be73a2f282440480cc905779dca487c69
KatalinaNadasi <katalinanadasi@gmail.com>
2023-05-05 11:29:57 +0200

    Add fake datas with json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Nav from './components/Nav'
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import theme from './style/theme';
@@ -20,11 +21,36 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export default function App() {
+  const [datas, setDatas] = useState([]);
+
+  const getData = () => {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:3030/clients", requestOptions)
+    .then((response) => response.json())
+    .then((result) => setDatas(result))
+    .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Nav />
+        {datas.map((data) => (
+          <div key={data.id}>
+            <h3>
+              <span>{data.id}</span> {data.name}
+            </h3>
+            <p>{data.contact}</p>
+          </div>
+        ))}
     </ThemeProvider>
   );
 }
